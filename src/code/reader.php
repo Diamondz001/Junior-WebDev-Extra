463d65d5a64769b9f5744d1a64805f8e4c8e0b2d
Diamondz001 <gwatt1337@gmail.com>
2016-07-15 15:51:22 +0100

    Created Reader class

---
+++ src/code/reader.php
@@ -0,0 +1,20 @@
+<?php
+
+class Reader
+{	
+	private $lines;
+
+	public function readTree($tree)
+	{
+		if ($handle)
+		{
+			while (($line = fgets($handle)) !== false)
+			{
+			}
+		} else
+		{
+			echo "Cannot read file/n";
+		}
+		
+	}
+}
