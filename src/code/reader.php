71a3beaed36001cce66fe455e159c9abf8a726ca
Diamondz001 <gwatt1337@gmail.com>
2016-07-15 15:52:23 +0100

    Added comments to reader

--- src/code/reader.php
+++ src/code/reader.php
@@ -2,15 +2,22 @@
 
 class Reader
 {	
+	private $_count = 0; //Counts the number of the current line.
 	private $lines;
 
 	public function readTree($tree)
 	{
+		$handle = fopen($tree,"r"); //Open the file.
+		$lines[0][0] = fgetc($handle); //Manually set the first value in the file to the array.
 		if ($handle)
 		{
 			while (($line = fgets($handle)) !== false)
 			{
+				$lines[$_count] = array_map('trim',explode(" ", $line)); //Add each number to the multi array.
+				$_count++; //Increment the line counter.
 			}
+			fclose($handle); //Close the file.
+			return $lines; //Return the new array.
 		} else
 		{
 			echo "Cannot read file/n";
